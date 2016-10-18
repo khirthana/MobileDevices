b0f051bb09a8f229f16958b78493080c78873ecf
khirthana <khirthana.subramanian@uoit.net>
2016-10-18 13:50:33 -0400

    updated assignment1

--- CSCI4100U-Assignment1/app/src/main/java/com/example/a100453865/csci4100u_assignment1/AskQuestion.java
+++ CSCI4100U-Assignment1/app/src/main/java/com/example/a100453865/csci4100u_assignment1/AskQuestion.java
@@ -2,6 +2,8 @@ package com.example.a100453865.csci4100u_assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AskQuestion extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class AskQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_question);
+    }
+
+    //if yes button is clicked
+    public void yesButton(View v) {
+
+    }
+
+    //if no button is clicked
+    public void noButton(View v) {
+
     }
 }
--- CSCI4100U-Assignment1/app/src/main/java/com/example/a100453865/csci4100u_assignment1/MainMenu.java
+++ CSCI4100U-Assignment1/app/src/main/java/com/example/a100453865/csci4100u_assignment1/MainMenu.java
@@ -2,12 +2,28 @@ package com.example.a100453865.csci4100u_assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
+
+    String[] questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        questions = getResources().getStringArray(R.array.questions);
+    }
+
+
+    //if start button is clicked in main layout then AskQuestion layout is displayed
+    public void questionLayout(View v) {
+        setContentView(R.layout.activity_ask_question);
+
+        //question is displayed on AskQuestion layout textView
+        TextView textView = (TextView)findViewById(R.id.questionTextView);
+
+        textView.setText(questions[0]);
     }
 }
