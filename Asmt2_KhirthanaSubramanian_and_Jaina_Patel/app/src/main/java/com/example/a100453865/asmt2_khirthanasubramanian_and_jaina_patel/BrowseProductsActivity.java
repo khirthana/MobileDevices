76330b0adea584e38c00f199842406df9e1e9c4b
khirthana <khirthana.subramanian@uoit.net>
2016-11-09 15:16:38 -0500

    updated browse_product_activity & layout

--- Asmt2_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/com/example/a100453865/asmt2_khirthanasubramanian_and_jaina_patel/BrowseProductsActivity.java
+++ Asmt2_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/com/example/a100453865/asmt2_khirthanasubramanian_and_jaina_patel/BrowseProductsActivity.java
@@ -1,36 +1,14 @@
-/*
- *  use the query function in the database helper to find all products
-
-o Create a function to display a product (showProduct)
--The function takes a Product as an argument
--The name, description, and price for the first product found is displayed in their respective text fields
--The price is submitted to a web service to find the corresponding price in BitCoin
--The BitCoin price is displayed in the corresponding text field
-
-
-o Once the products are loaded, call showProduct() with the first product
-
-
-o When “Next” is clicked, call showProduct() with the next product
--If there are no further products, disable the “Next” button
-
-
-o When “Prev” is clicked, call showProduct() with the previous product
--If there are no earlier products, disable the “Previous” button
-
-
-o When “Delete” is clicked, call the database helper function to delete the product
-- query the database again to get all of the products again
-
-
-o When “Add New Product” is selected (from the menu), invoke the AddProductActivity
-
- */
-
 package com.example.a100453865.asmt2_khirthanasubramanian_and_jaina_patel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class BrowseProductsActivity extends AppCompatActivity {
 
@@ -38,5 +16,65 @@ public class BrowseProductsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browse_products_activity);
+
+        //textfields are set to uneditable
+        EditText editName= (EditText) findViewById(R.id.editName);
+        editName.setEnabled(false);
+
+        EditText editDescription= (EditText) findViewById(R.id.editDescription);
+        editDescription.setEnabled(false);
+
+        EditText editPriceDollar= (EditText) findViewById(R.id.editPriceDollar);
+        editPriceDollar.setEnabled(false);
+
+        EditText editPriceBitcoin= (EditText) findViewById(R.id.editPriceBitcoin);
+        editPriceBitcoin.setEnabled(false);
+
+        // use the query function in the database helper to find all products
+
+        //Once the products are loaded, call showProduct() with the first produc
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //when 'Add New Product' is selected from menu, AddProductActivity is started
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent(BrowseProductsActivity.this,AddProductActivity.class);
+        startActivity(i);
+        return true;
+
+    }
+
+    //function to display a product; takes a Product as an argument
+    void showProduct(Product p){
+
+        /*-The name, description, and price for the first product found is displayed in their respective text fields
+
+        -The price is submitted to a web service to find the corresponding price in BitCoin
+
+        -The BitCoin price is displayed in the corresponding text field*/
+
+
+    }
+    //when prev button is clicked, calls showProduct() with the previous product
+    public void prevProduct(View view){
+
+        //If there are no earlier products, “Previous” button is disabled
+    }
+
+    //when next button is clicked, calls showProduct() with the next product
+    public void nextProduct(View view){
+
+        //If there are no further products, “Next” button is disabled
+    }
+
+    //when delete button is clicked, calls the database helper function to delete the product
+    public void deleteProduct(View view){
+
+    }
+
 }
--- Asmt2_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/com/example/a100453865/asmt2_khirthanasubramanian_and_jaina_patel/Product.java
+++ Asmt2_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/com/example/a100453865/asmt2_khirthanasubramanian_and_jaina_patel/Product.java
@@ -1,9 +1,13 @@
-/*
- *  stores the productId, name, description, and price of a product
- */
-
 package com.example.a100453865.asmt2_khirthanasubramanian_and_jaina_patel;
 
 
 public class Product {
+
+    /* stores
+    -productId (integer, primary key)
+    -name (string)
+    -description (string)
+    -price (decimal)
+     */
+
 }
