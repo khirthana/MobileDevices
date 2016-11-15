0bf5c5a5cc66a3db5ed43e5a70701f1a5313ae86
Jaina Patel <jaina.patel@uoit.net>
2016-11-15 13:51:40 -0500

    Update Product.java

--- Asmt2_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/com/example/a100453865/asmt2_khirthanasubramanian_and_jaina_patel/Product.java
+++ Asmt2_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/com/example/a100453865/asmt2_khirthanasubramanian_and_jaina_patel/Product.java
@@ -6,4 +6,23 @@ package com.example.a100453865.asmt2_khirthanasubramanian_and_jaina_patel;
 
 
 public class Product {
+    int product_id;
+    String product_name;
+    String product_desc;
+    double price;
+
+
+    public Product (){
+
+    }
+
+    public Product (int product_id,String product_name, String product_desc, double price){
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_desc = product_desc;
+        this.price = price;
+    }
+
+
 }
+
