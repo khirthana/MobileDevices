da38aadd204d2b28bc8e89eddf3ecbdbec22d182
khirthana <khirthana.subramanian@uoit.net>
2016-11-24 00:00:52 -0500

    updated trips,aboutus,addnote,notes,note layouts

--- TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/Notes.java
+++ TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/Notes.java
@@ -1,18 +1,13 @@
 package csci4100u.travellogger_project_khirthanasubramanian_and_jainapatel;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
-public class Home extends AppCompatActivity {
+public class Notes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
+        setContentView(R.layout.activity_notes);
     }
 }
