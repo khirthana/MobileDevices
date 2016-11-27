f635f805ce03447956434a554a60278b96813032
Jaina Patel <jaina.patel@uoit.net>
2016-11-26 22:28:58 -0500

    Update Home.java

--- TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/Home.java
+++ TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/Home.java
@@ -7,19 +7,163 @@
  */
 package csci4100u.travellogger_project_khirthanasubramanian_and_jainapatel;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class Home extends AppCompatActivity implements LocationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textTime);
+
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+        textView.setText(currentDateTimeString);
 
 
+        setupLocationServices();
+    }
+
+    private void setupLocationServices() {
+        requestLocationPermissions();
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // request that the user install the GPS provider
+            String locationConfig = Settings.ACTION_LOCATION_SOURCE_SETTINGS;
+            Intent enableGPS = new Intent(locationConfig);
+            startActivity(enableGPS);
+        } else {
+            updateLocation();
+        }
+    }
+
+    private void updateLocation() {
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+            // request an fine location provider
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setSpeedRequired(false);
+            criteria.setCostAllowed(false);
+            String recommended = locationManager.getBestProvider(criteria, true);
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+
+            Location location = locationManager.getLastKnownLocation(recommended);
+            if (location != null) {
+                showLocationName(location);
+            }
+        } else {
+            Log.d("LocationSample", "Location provider permission denied, perms: " + ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION));
+        }
+    }
+
+    final int PERMISSIONS_REQUEST_ACCESS_LOCATION = 410020;
+
+    private void requestLocationPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                }
+
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_LOCATION);
+
+                return;
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateLocation();
+                } else {
+                }
+                return;
+            }
+        }
+    }
+
+    public void onProviderEnabled(String provider) {
+        Log.d("LocationSample", "onProviderEnabled(" + provider + ")");
+    }
+
+    public void onProviderDisabled(String provider) {
+        Log.d("LocationSample", "onProviderDisabled(" + provider + ")");
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("LocationSample", "onStatusChanged(" + provider + ", " + status + ", extras)");
+    }
+
+    public void onLocationChanged(Location location) {
+        Log.d("LocationSample", "onLocationChanged(" + location + ")");
+
+        showLocationName(location);
+    }
+
+    private void showLocationName(Location location) {
+        Log.d("LocationSample", "showLocationName("+location+")");
+
+        if (Geocoder.isPresent()) {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+            try {
+                List<Address> ls = geocoder.getFromLocation(location.getLatitude(),
+                        location.getLongitude(),
+                        1);
+
+                for (Address addr: ls) {
+
+                    String city = addr.getLocality();
+
+
+                    TextView city_field = (TextView)findViewById(R.id.textCity);
+                    city_field.setText(city);
+                    city_field.setEnabled(false);
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.d("LocationSample", "No geocoder present");
+        }
     }
 }
