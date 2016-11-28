35a99ce4e659ba1afd921d09fa608f94d65764e7
khirthana <khirthana.subramanian@uoit.net>
2016-11-27 23:38:14 -0500

    started menu

--- TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/AboutUs.java
+++ TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/AboutUs.java
@@ -8,14 +8,62 @@
  */
 package csci4100u.travellogger_project_khirthanasubramanian_and_jainapatel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class AboutUs extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent i = new Intent(AboutUs.this,Home.class);
+                //i.putExtra("list", (Serializable) product_list);
+                startActivity(i);
+                return true;
+            case R.id.nav_notes:
+                Intent i2 = new Intent(AboutUs.this,Notes.class);
+                //i2.putExtra("list", (Serializable) product_list);
+                startActivity(i2);
+                return true;
+            case R.id.nav_wishist:
+                Intent i3 = new Intent(AboutUs.this,Wishlist.class);
+                startActivity(i3);
+                return true;
+            case R.id.nav_instructions:
+                Intent i4 = new Intent(AboutUs.this,About.class);
+                startActivity(i4);
+                return true;
+            case R.id.nav_about_us:
+                Intent i5 = new Intent(AboutUs.this,AboutUs.class);
+                startActivity(i5);
+                return true;
+            case R.id.nav_settings:
+                Intent i6 = new Intent(AboutUs.this,About.class);
+                startActivity(i6);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
+
+
--- TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/Home.java
+++ TravelLogger_Project_KhirthanaSubramanian_and_Jaina_Patel/app/src/main/java/csci4100u/travellogger_project_khirthanasubramanian_and_jainapatel/Home.java
@@ -37,7 +37,7 @@ public class Home extends AppCompatActivity implements LocationListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         TextView textView = (TextView) findViewById(R.id.textTime);
 
